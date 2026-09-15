09bb8c98b4be88ff81ceda04ca5bc0134750f5a3
oscarrojasgu <oscarrojasgu@hotmail.com>
2026-09-15 11:50:06 -0500

    Update page.tsx

--- app/players/[id]/page.tsx
+++ app/players/[id]/page.tsx
@@ -284,14 +284,18 @@ export default async function PlayerPage({ params }: PlayerPageProps) {
       {/* HEADER */}
 
       <nav
-        style={{
-          display: "flex",
-          alignItems: "center",
-          padding: "18px 32px",
-          borderBottom: "1px solid #e5e5e5",
-          background: "#fff",
-        }}
-      >
+  style={{
+    position: "sticky",
+    top: 0,
+    zIndex: 1000,
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    padding: "18px 32px",
+    borderBottom: "1px solid #e5e5e5",
+    background: "#fff",
+  }}
+>
         <Link
           href="/"
           style={{
